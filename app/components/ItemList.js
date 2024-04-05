3f0dcf24916247bdd90def2305714a27470a7e4e
specter40 <skj92852@uga.edu>
2024-04-05 11:10:17 -0400

    Added some mapping functions to the ItemList component.

--- app/components/ItemList.js
+++ app/components/ItemList.js
@@ -1,19 +1,23 @@
-
+"use client";
+import  React from 'react';
+import Card from './Card.js';
+import Item from './Item.js';
 
 const ItemList = (props) => {
     return (
         <Card className="user">
-        {props.userList.map((item) => (
-            <Item 
-                day={item.day}
-                activity={item.activity}
-                time={item.time}
-                calories={item.calories}
-                heart={item.heart}          
-            />
-
-            
-        ))}
+            {props.listItems.map((item) => (
+                <Item 
+                    key={item.id}
+                    day={item.day}
+                    activity={item.activity}
+                    time={item.time}
+                    calories={item.calories}
+                    heart={item.heart}          
+                />           
+            ))}
         </Card>
     );
 }
+
+export default ItemList;
