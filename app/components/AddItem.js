f2b20ae95d276d698bada6e95e67c0c93c767484
Swathi Ramaswamy <swathiaramaswamy@gmail.com>
2024-04-05 11:21:08 -0400

    Swathi R. - more AddItem css work Need to do: (1) make colums

--- app/components/AddItem.js
+++ app/components/AddItem.js
@@ -10,92 +10,156 @@ const AddItem = () => {
     return (
         <div id="page">
             <div id="main_form">
-                <label for="date">Date:</label>
-                <input id="date"
-                    type="date"  
-                />
+                <div>
+                    <label for="date" class="date_style">Date:</label>
+                    <input id="date" class="date_style"
+                        type="date"  
+                    />
+                    <br />
+                </div>
 
-                <label for="exercise_type">What kind of exercise did you do?</label>
-                <select id="exercise_type">
-                    <option>Weight Training</option>
-                    <option>Running</option>
-                    <option>Stretching</option>
-                    <option>Walking</option>
-                    <option>Jogging</option>
-                    <option>Cycling</option>
-                    <option>Swimming</option>
-                    <option>Hiking</option>
-                    <option>Kickboxing</option>
-                </select>
+                <div>
+                    <label for="exercise_type">What kind of exercise did you do?</label>
+                    <br />
+                    <select multiple id="exercise_type">
+                        <option>Weight Training</option>
+                        <option>Running</option>
+                        <option>Stretching</option>
+                        <option>Walking</option>
+                        <option>Jogging</option>
+                        <option>Cycling</option>
+                        <option>Swimming</option>
+                        <option>Hiking</option>
+                        <option>Kickboxing</option>
+                    </select>
 
-                <label for="indoor_outdoor">Indoor or Outdoor Run, Walk, or Jog?</label>
-                <select id="indoor_outdoor">
-                    <option>Indoor</option>
-                    <option>Outdoor</option>
-                </select>
+                    <br />
+                </div>
 
-                <label for="distance">Distance for Run, Walk, or Jog?</label>
-                <input id="distance"
-                    type="number" 
-                />
-                <span>mi</span>
+                <div>
+                    <label for="indoor_outdoor">Indoor or Outdoor Run, Walk, or Jog?</label>
+                    <br />
+                    <select id="indoor_outdoor">
+                        <option>Indoor</option>
+                        <option>Outdoor</option>
+                    </select>
 
-                <label for="calories_burned">Calories burned:</label>
-                <input id="calories_burned"
-                    type="number"
-                />
-                <span>calories</span>
+                    <br />
+                </div>
 
-                <label for="weight_train_type">What type of weight training?</label>
-                <select multiple id="weight_train_type">
-                    <option>1</option>
-                </select>
+                <div>
+                    <label for="distance">Distance for Run, Walk, or Jog?</label>
+                    <br />
+                    <input id="distance"
+                        type="number" 
+                    />
+                    <span> mi</span>
 
-                <label for="weight">Weight Used:</label>
-                <input id="weight"
-                    type="number"
-                />
-                <span>lbs</span>
+                    <br />
+                </div>
+            
 
-                <label for="reps_and_sets">How many sets and reps did you do?</label>
-                <input id="sets"
-                    type="number"
-                />
-                <span> sets of </span>
-                <input id="reps"
-                    type="number"
-                />
-                <span>reps</span>
+                <div>
+                    <label for="calories_burned">Calories burned:</label>
+                    <br />
+                    <input id="calories_burned"
+                        type="number"
+                    />
+                    <span> calories</span>
 
-                <label for="distance_hiking">Distance for hiking</label>
-                <input id="distance_hiking"
-                    type="number"
-                />
-                <span>mi</span>
+                    <br />
+                </div>
 
-                <label for="moderate_advanced">Moderate or Advanced hiking?</label>
-                <select id="moderate_advanced">
-                    <option>Moderate</option>
-                    <option>Advanced</option>
-                </select>
+                <div>
+                    <label for="weight_train_type">What type of weight training?</label>
+                    <br />
+                    <select multiple id="weight_train_type">
+                        <option>1</option>
+                    </select>
 
-                <label for="indoor_outdoor_cycling">Indoor or Outdoor Cycling?</label>
-                <select id="indoor_outdoor_cycling">
-                    <option>Indoor</option>
-                    <option>Outdoor</option>
-                </select>
+                    <br />
+                </div>
 
-                <label for="distance_cycling">Distance Cycling?</label>
-                <input id="distance_cycling"
-                    type="number"
-                />
-                <span>mi</span>
+                <div>
+                    <label for="weight">Weight Used:</label>
+                    <br />
+                    <input id="weight"
+                        type="number"
+                    />
+                    <span> lbs</span>
 
-                <label for="distance_swimming">Distance Cycling?</label>
-                <input id="distance_swimming"
-                    type="number"
-                />
-                <span>mi</span>
+                    <br />
+                </div>
+
+                <div>
+                    <label for="reps_and_sets">How many sets and reps did you do?</label>
+                    <br />
+                    <input id="sets"
+                        type="number"
+                    />
+                    <span> sets of </span>
+                    <input id="reps"
+                        type="number"
+                    />
+                    <span> reps</span>
+
+                    <br />
+                </div>
+
+                <div>
+                    <label for="distance_hiking">Distance for hiking</label>
+                    <br />
+                    <input id="distance_hiking"
+                        type="number"
+                    />
+                    <span> mi</span>
+
+                    <br />
+                </div>
+
+                <div>
+                    <label for="moderate_advanced">Moderate or Advanced hiking?</label>
+                    <br />
+                    <select id="moderate_advanced">
+                        <option>Moderate</option>
+                        <option>Advanced</option>
+                    </select>
+
+                    <br />
+                </div>
+
+                <div>
+                    <label for="indoor_outdoor_cycling">Indoor or Outdoor Cycling?</label>
+                    <br />
+                    <select id="indoor_outdoor_cycling">
+                        <option>Indoor</option>
+                        <option>Outdoor</option>
+                    </select>
+
+                    <br />
+                </div>
+
+                <div>
+                    <label for="distance_cycling">Distance Cycling?</label>
+                    <br />
+                    <input id="distance_cycling"
+                        type="number"
+                    />
+                    <span> mi</span>
+
+                    <br />
+                </div>
+
+                <div>
+                    <label for="distance_swimming">Distance Cycling?</label>
+                    <br />
+                    <input id="distance_swimming"
+                        type="number"
+                    />
+                    <span> mi</span>
+
+                    <br />
+                </div>
             </div>
         </div>
     );
