d7d2e05b51b2766820dec60b3740a95310d4152a
Swathi Ramaswamy <swathiaramaswamy@gmail.com>
2024-04-05 09:34:37 -0400

    Swathi R. - minor Changes

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import styles from "./page.module.css";
 import Header from "./components/Header";
 import Homepage from "./components/Homepage";
+import AddItem from "./components/AddItem";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Homepage />
+      <AddItem />
     </div>
   );
 }
