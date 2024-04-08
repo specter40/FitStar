00efdd021eb66c228d4f655ed2e3a712293b0ff5
allyjonesn <anj04632@uga.edu>
2024-04-08 19:00:59 -0400

    Revert "Update AddItem.js"
    This reverts commit 8378f554a93ea8accb7b0c484f9f877dfc4511b0.

--- app/components/AddItem.js
+++ app/components/AddItem.js
@@ -9,187 +9,108 @@ import '../css/AddItem.css';
 const AddItem = () => {
 
     return (
-        <div id="page">
-            <div id="main_form">
-                <div class="chunk">
-                <div id="date_container" class="chunk">
-                    <label for="date" class="date_style">Date:</label>
-                    <input id="date" class="date_style"
-                        type="date"  
-                    />
-                    <br />
-                </div>
+        <div>
+            <label for="date">Date:</label>
+            <input id="date"
+                type="date"  
+            />
 
-                <div id="column_container">
-                    <div class="column">
-                        <div class="chunk">
-                            <label for="exercise_type">What kind of exercise did you do?</label>
-                            <br />
-                            <select multiple id="exercise_type">
-                                <option>Weight Training</option>
-                                <option>Running</option>
-                                <option>Stretching</option>
-                                <option>Walking</option>
-                                <option>Jogging</option>
-                                <option>Cycling</option>
-                                <option>Swimming</option>
-                                <option>Hiking</option>
-                                <option>Kickboxing</option>
-                            </select>
+            <label for="exercise_type">What kind of exercise did you do?</label>
+            <select id="exercise_type">
+                <option>Weight Training</option>
+                <option>Running</option>
+                <option>Stretching</option>
+                <option>Walking</option>
+                <option>Jogging</option>
+                <option>Cycling</option>
+                <option>Swimming</option>
+                <option>Hiking</option>
+                <option>Kickboxing</option>
+            </select>
 
-                            <br />
-                        </div>
+            <label for="indoor_outdoor">Indoor or Outdoor Run, Walk, or Jog?</label>
+            <select id="indoor_outdoor">
+                <option>Indoor</option>
+                <option>Outdoor</option>
+            </select>
 
-                        <div class="chunk">
-                            <label for="indoor_outdoor">Indoor or Outdoor Run, Walk, or Jog?</label>
-                            <br />
-                            <select id="indoor_outdoor">
-                                <option>Indoor</option>
-                                <option>Outdoor</option>
-                            </select>
+            <label for="distance">Distance for Run, Walk, or Jog?</label>
+            <input id="distance"
+                type="number" 
+            />
+            <span>mi</span>
 
-                            <br />
-                        </div>
+            <label for="calories_burned">Calories burned:</label>
+            <input id="calories_burned"
+                type="number"
+            />
+            <span>calories</span>
 
-                        <div class="chunk">
-                            <label for="distance">Distance for Run, Walk, or Jog?</label>
-                            <br />
-                            <input id="distance"
-                                type="number" 
-                            />
-                            <span> mi</span>
+            <label for="weight_train_type">What type of weight training?</label>
+            <select id="weight_train_type">
+                <option>Traditional Strength Training</option>
+                <option>Functional Weight Training </option>
+            </select>
 
-                            <br />
-                        </div>
-                    
+            <label for="body_train_type">Which part of your body did you train?</label>
+            <select multiple id="body_train_type">
+                <option>Legs - Quads</option>
+                <option>Legs - Hamstrings </option>
+                <option>Legs - Calves</option>
+                <option>Legs - Glutes</option>
+                <option>Core Strength - Abs</option>
+                <option>Arms - Biceps</option>
+                <option>Arms - Triceps</option>
+                <option>Arms - Shoulders</option>
+                <option>Arms</option>
+            </select>
 
-                        <div class="chunk">
-                            <label for="calories_burned">Calories burned:</label>
-                            <br />
-                            <input id="calories_burned"
-                                type="number"
-                            />
-                            <span> calories</span>
+            <label for="weight">Weight Used:</label>
+            <input id="weight"
+                type="number"
+            />
+            <span>lbs</span>
 
-                            <br />
-                        </div>
-                        
-                    </div>
+            <label for="reps_and_sets">How many sets and reps did you do?</label>
+            <input id="sets"
+                type="number"
+            />
+            <span> sets of </span>
+            <input id="reps"
+                type="number"
+            />
+            <span>reps</span>
 
-                    <div class="column">
+            <label for="distance_hiking">Distance for hiking</label>
+            <input id="distance_hiking"
+                type="number"
+            />
+            <span>mi</span>
 
-                        <div class="chunk">
-                            <label for="weight_train_type">What type of weight training?</label>
-                            <br />
-                            <select multiple id="weight_train_type">
-                                <option>Legs - Quads</option>
-                                <option>Legs - Hamstrings </option>
-                                <option>Legs - Calves</option>
-                                <option>Legs - Glutes</option>
-                                <option>Core Strength - Abs</option>
-                                <option>Arms - Biceps</option>
-                                <option>Arms - Triceps</option>
-                                <option>Arms - Shoulders</option>
-                                <option>Arms - Forearms</option>
-                            </select>
+            <label for="moderate_advanced">Moderate or Advanced hiking?</label>
+            <select id="moderate_advanced">
+                <option>Moderate</option>
+                <option>Advanced</option>
+            </select>
 
-                            <br />
-                        </div>
+            <label for="indoor_outdoor_cycling">Indoor or Outdoor Cycling?</label>
+            <select id="indoor_outdoor_cycling">
+                <option>Indoor</option>
+                <option>Outdoor</option>
+            </select>
 
-                    </div>
+            <label for="distance_cycling">Distance Cycling?</label>
+            <input id="distance_cycling"
+                type="number"
+            />
+            <span>mi</span>
 
-                    <div class="column">
-                
-                        <div class="chunk">
-                            <label for="weight">Weight Used:</label>
-                            <br />
-                            <input id="weight"
-                                type="number"
-                            />
-                            <span> lbs</span>
+            <label for="distance_swimming">Distance Cycling?</label>
+            <input id="distance_swimming"
+                type="number"
+            />
+            <span>mi</span>
 
-                            <br />
-                        </div>
-
-                        <div class="chunk">
-                            <label for="reps_and_sets">How many sets and reps did you do?</label>
-                            <br />
-                            <input id="sets"
-                                type="number"
-                            />
-                            <span> sets of </span>
-                            <input id="reps"
-                                type="number"
-                            />
-                            <span> reps</span>
-
-                            <br />
-                        </div>
-
-                        <div class="chunk">
-                            <label for="distance_hiking">Distance for hiking</label>
-                            <br />
-                            <input id="distance_hiking"
-                                type="number"
-                            />
-                            <span> mi</span>
-
-                            <br />
-                        </div>
-
-                    </div>
-
-                    <div class="column">
-
-                        <div class="chunk">
-                            <label for="moderate_advanced">Moderate or Advanced hiking?</label>
-                            <br />
-                            <select id="moderate_advanced">
-                                <option>Moderate</option>
-                                <option>Advanced</option>
-                            </select>
-
-                            <br />
-                        </div>
-
-                        <div class="chunk">
-                            <label for="indoor_outdoor_cycling">Indoor or Outdoor Cycling?</label>
-                            <br />
-                            <select id="indoor_outdoor_cycling">
-                                <option>Indoor</option>
-                                <option>Outdoor</option>
-                            </select>
-
-                            <br />
-                        </div>
-
-                        <div class="chunk">
-                            <label for="distance_cycling">Distance Cycling?</label>
-                            <br />
-                            <input id="distance_cycling"
-                                type="number"
-                            />
-                            <span> mi</span>
-
-                            <br />
-                        </div>
-
-                        <div class="chunk">
-                            <label for="distance_swimming">Distance Cycling?</label>
-                            <br />
-                            <input id="distance_swimming"
-                                type="number"
-                            />
-                            <span> mi</span>
-
-                            <br />
-                        </div>
-                    </div>
-                </div>
-
-                <button id="add_info">Add to Activity</button>
-            </div>
-        </div>
         </div>
     );
 
