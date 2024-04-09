08ea934280ba79a94a17737c5c0331699a7d7a4d
Karyn Huang <karyn.huang689@gmail.com>
2024-04-08 21:16:23 -0400

    Sign in update on css

--- app/page.js
+++ app/page.js
@@ -9,7 +9,6 @@ export default function Home() {
     <div>
       <Header loggedIn = {false}/>
       <Homepage />
-      <SignIn />
     </div>
   );
 }
--- app/signIn/SignIn.js
+++ app/signIn/SignIn.js
@@ -22,15 +22,18 @@ const SignIn = (props) => {
     }
     return(
         <div className="page">
-            <h1>Welcome</h1>
-            <img className="logo" src='/images/Frame.png' id="titleLogo"alt="Logo" />
-            <h2>Login</h2>
-            <h3>Username</h3>
-            <input id='username' value={username} onChange={(event) => setUsername(event.target.value)} />
-            <h3>Password</h3>
-            <input id='password' type="password" value={password} onChange= {(event) => setPassword(event.target.value)}/>
-            <button onClick={handleLogin}>Login</button>
-            <a onClick={createHandler}>Create a new account</a>
+            <div className="container">
+                <img src="/images/Star.png" id="star"/>
+                <h1>Welcome</h1>
+                <img className="logo" src='/images/Frame.png' id="titleLogo"alt="Logo" />
+                <h2>Login</h2>
+                <h3>Username</h3>
+                <input id='username' value={username} onChange={(event) => setUsername(event.target.value)} />
+                <h3>Password</h3>
+                <input id='password' type="password" value={password} onChange= {(event) => setPassword(event.target.value)}/>
+                <button onClick={handleLogin}>Login</button>
+                <a onClick={createHandler}>Create a new account</a>
+            </div>
         </div>
     );
 };
