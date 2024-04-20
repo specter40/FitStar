92b5c49f6fdbbe8d2dc3cd2b3ab00c255016c18c
Karyn Huang <karyn.huang689@gmail.com>
2024-04-20 01:09:53 -0400

    Create Account page
    - installed axios
    - installed bcryptjs
    - installed jsonwebtoken
    - created folders for UserContext
    - added users.js to api folder
    - edited and rerouted the Login/Create Account tab (it now only goes to create account page, but once the sign in page is done, we'll need to reroute it to sign in page first)
    - added some css for the CreateAccount page (in progress)
    - added a schema called User.js in the model folder

--- app/createAccount/CreateAccount.js
+++ app/createAccount/CreateAccount.js
@@ -1,33 +1,87 @@
-"use client"
-import './css/CreateAccount.css';
+import { useState } from 'react';
+import axios from 'axios';
+import '../createAccount/css/CreateAccount.css';
 
 const CreateAccount = () => {
-    let username = '';
-    let password = '';
-    let email = '';
+    // State to manage form inputs
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    // Function to handle form input changes
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
 
     // Function to handle form submission
-    const handleCreateAccount = async () => {
-        if (username.trim() !== '' && email.trim() !== '' && password.trim() !== '') {
-            // Redirect to the login page after account creation
-            router.push('/SignIn');
-        } else {
-            alert('Please fill in all fields');
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try{
+            await axios.post('/api/users', formData);
+            console.log(res.data);
+        } catch (err) {
+            console.error(err);
         }
     };
 
     return (
-        <div className="page">
-            <h1>Create New Account</h1>
-            <h3>Username</h3>
-            <input id='username' value={username} onChange={(e) => setUsername(e.target.value)} />
-            <h3>Email</h3>
-            <input id='email' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <h3>Password</h3>
-            <input id='password' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleCreateAccount}>Create Account</button>
+        <div className='page'>
+            <form onSubmit={ handleSubmit }>
+                <div className='starContainer'>
+                    <img className="star" src='/images/Star.png' alt="Star" />
+                    <h1>Welcome</h1>
+                </div>
+                <div className='bodyContainer'>
+                    <img className="logo" src='/images/Logo.png' alt="Logo" />
+                    <h2>Create New Account</h2>
+                    <h3>Username</h3>
+                    <input type="text" name="username" value={formData.username} onChange={ handleChange } />
+                    <h3>Email</h3>
+                    <input type="email" name="email" value={formData.email} onChange={ handleChange } />
+                    <h3>Password</h3>
+                    <input type="password" name="password" value={formData.password} onChange={ handleChange } />
+                    <button className='accountBt' type="submit">Create Account</button>
+                </div>
+            </form>
         </div>
     );
 };
 
 export default CreateAccount;
+
+// "use client"
+// import './css/CreateAccount.css';
+
+// const CreateAccount = () => {
+//     let username = '';
+//     let password = '';
+//     let email = '';
+
+//     // Function to handle form submission
+//     const handleCreateAccount = async () => {
+//         if (username.trim() !== '' && email.trim() !== '' && password.trim() !== '') {
+//             // Redirect to the login page after account creation
+//             router.push('/SignIn');
+//         } else {
+//             alert('Please fill in all fields');
+//         }
+//     };
+
+//     return (
+//         <div className="page">
+//             <h1>Create New Account</h1>
+//             <h3>Username</h3>
+//             <input id='username' value={username} onChange={(e) => setUsername(e.target.value)} />
+//             <h3>Email</h3>
+//             <input id='email' type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+//             <h3>Password</h3>
+//             <input id='password' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+//             <button onClick={handleCreateAccount}>Create Account</button>
+//         </div>
+//     );
+// };
+
+// export default CreateAccount;
--- app/createAccount/page.js
+++ app/createAccount/page.js
@@ -1,6 +1,8 @@
+"use client"
 import "../globals.css";
 import Header from "../components/Header";
 import CreateAccount from "./CreateAccount";
+import axios from 'axios';
 
 export default function Home() {
     return (
